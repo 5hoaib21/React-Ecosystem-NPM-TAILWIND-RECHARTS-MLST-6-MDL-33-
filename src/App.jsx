99edb8a728d4721ad0a6ce51b26cd43fddccbd38
MD ABDUR RAHMAN SHOAIB <mdabdurrahmanshoaib@gmail.com>
2026-03-18 05:15:18 +0600

    lucide React packege added

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+// import DaisyNav from "./components/DaisyNav/DaisyNav";
+
+function App() {
+  return (
+    <>
+
+<header>
+<Navbar></Navbar>
+  <DaisyNav></DaisyNav>
+</header>
+
+      <h1 className="p-20 text-4xl font-bold">Hey tailwind + DaisyUI </h1>
+     
+    </>
+  );
+}
+import DaisyNav from "./components/DaisyNav/DaisyNav";
+import Navbar from "./Navbar/Navbar";
+
+export default App;
