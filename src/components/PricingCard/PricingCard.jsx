94f6398fc90530cd0b815c41d901c9231e2b93f9
MD ABDUR RAHMAN SHOAIB <mdabdurrahmanshoaib@gmail.com>
2026-03-18 14:42:49 +0600

    Use Daisy UI pricing card to modify for the component

--- src/components/PricingCard/PricingCard.jsx
+++ src/components/PricingCard/PricingCard.jsx
@@ -4,14 +4,14 @@ import PricingFeatures from './PricingFeatures';
 const PricingCard = ({pricing}) => {
   const {name, price, description, features} = pricing;
   return (
-    <div className='border p-4 bg-gray-700 rounded-3xl'>
+    <div className='flex flex-col  border p-4 bg-gray-700 rounded-3xl'>
       {/* card header */}
       <div>
         <h1 className='text-6xl'>{name}</h1>
         <h4 className='text-3xl'>${price}</h4>
       </div>
       {/* card body */}
-      <div className='bg-pink-900 p-3 rounded-3xl mt-5'>
+      <div className='bg-pink-900 p-3 rounded-3xl mt-5 flex-1'>
       <p>{description}</p>
       {
         features.map((feature,i) => <PricingFeatures
@@ -19,6 +19,7 @@ const PricingCard = ({pricing}) => {
          feature={feature}></PricingFeatures>)
       }
       </div>
+      <button className="btn w-full mt-5">Subscribe !</button>
     </div>
   );
 };
